c597134a2720bad9d0b4e035a1828d8c8053cf7f
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-11-27 17:20:49 +0100

    Created carrasco command

--- lib/simple_build/command_builder.rb
+++ lib/simple_build/command_builder.rb
@@ -2,32 +2,13 @@ require "thor"
 
 module SimpleBuild
   class CommandBuilder
-    def definitions(file = nil)
-      {
-        commands: {
-          phpunit: {
-            help: "phpunit ARGS",
-            description: "A description for phpunit",
-            command: "phpunit --version"
-          },
-          phpcs: {
-            help: "phpcs ARGS",
-            description: "A description for php cs",
-            command: "phpcs --version"
-          }
-        }
-      }
-    end
-
-    def from_file(file = nil)
-
+    def from_config(config)
       klass = Class.new(Thor)
 
-
-      definitions[:commands].each do |method, options|
-        help        = options.fetch(:help)
-        description = options.fetch(:description)
-        command     = options.fetch(:command)
+      config['commands'].each do |method, options|
+        help        = options.fetch('help')
+        description = options.fetch('description')
+        command     = options.fetch('command')
 
         klass.class_eval do
           desc help, description
