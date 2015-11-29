a8f4bb26e277fe067cc589bda52a1e8d740a072d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-11-29 23:44:19 +0100

    Added code coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,30 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'carrasco'
 
+def simplecov
+  require "simplecov"
+
+  SimpleCov.start do
+    add_filter "/test/"
+    add_group "lib", "lib"
+  end
+end
+
+if ENV['COVERALLS']
+  require "coveralls"
+  Coveralls.wear!
+  simplecov
+end
+
+if ENV['COVERAGE']
+  simplecov
+end
+
+if ENV['SCRUTINIZER']
+  require "scrutinizer/ocular"
+  Scrutinizer::Ocular.watch!
+  simplecov
+end
+
 require 'minitest/autorun'
 require 'mocha/mini_test'
