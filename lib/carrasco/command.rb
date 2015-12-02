f6dc9f2816d236cd198e293a657b8fc8ebc33f7e
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-12-02 15:10:29 +0100

    Improved command class

--- lib/carrasco/command.rb
+++ lib/carrasco/command.rb
@@ -3,11 +3,12 @@ require "thor/core_ext/hash_with_indifferent_access"
 module Carrasco
   class Command
     attr_reader :command_name, :command, :description, :help
+    alias_method :to_s, :command
 
     def initialize(command_name, options = {})
       options = Thor::CoreExt::HashWithIndifferentAccess.new(options)
       @command_name = command_name
-      @command      = options[:command]
+      @command      = options[:command] || command_name
       @help         = options[:help] || command_name
       @description  = options[:description] || "description not given"
     end
