6ea7ae48269bea61ca38a06a72c8ea212c35ddf4
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-12-03 17:50:00 +0100

    Fixed test

--- lib/carrasco/thor.rb
+++ lib/carrasco/thor.rb
@@ -4,15 +4,15 @@ module Carrasco
   class Thor < ::Thor
     no_commands do
       def execute_command(command)
-        command_executer.execute(command)
+        command_executer.execute!(command)
       end
 
       def command_executer=(command_executer)
-        @command_executer = command_executer
+        @@command_executer = command_executer
       end
 
       def command_executer
-        @command_executer ||= CommandExecuter.new
+        @@command_executer ||= CommandExecuter.new
       end
     end
   end
