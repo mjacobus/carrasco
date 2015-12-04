be43b4585a34eb4f8c2ace2e660742e5be8080df
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-12-04 16:35:24 +0100

    Improved testability

--- test/carrasco/command_builder_test.rb
+++ test/carrasco/command_builder_test.rb
@@ -2,15 +2,29 @@ require "test_helper"
 
 class CommandBuilderTest < Minitest::Test
   def test_can_inject_command_into_class
-    command = stub({ "command"      => "the_command",
-                "command_name" => "command_name",
-                "description"  => "the_description",
-                "help"         => "the_help" })
-
     klass = stub()
     klass.expects(:desc).with("the_help", "the_description")
     klass.expects(:class_eval)
 
     Carrasco::CommandBuilder.new.inject_command_into_class(command, klass)
+  end
+
+  def test_defines_command_in_the_class
+    klass = Class.new(::Thor)
+
+    Carrasco::CommandBuilder.new.inject_command_into_class(command, klass)
+
+    assert klass.new.respond_to?(:command_name)
+  end
+
+  private
+
+  def command
+    command = stub({
+      "command"      => "the_command",
+      "command_name" => "command_name",
+      "description"  => "the_description",
+      "help"         => "the_help"
+    })
   end
 end
